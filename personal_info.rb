3495bfdaf85eefdcf24a9d81c0df511ceedb0611
bmayle <bmayle17@gmail.comgit config --global user.name bmaylegit add . git config --global user.email you@example.comgit config --global user.email bmayle17@gmail.com>
2017-12-09 14:48:50 -0500

    2nd assertion

--- personal_info.rb
+++ personal_info.rb
@@ -5,8 +5,55 @@ def info()
 	nikolai = {:name => "Nikolai", :age => 9, :pets => "Yes", :color => "Blue"}
     kobe = {:name => "Kobe", :age => 8, :pets => "Yes", :color => "Red"}
 
+    if bobby[:name] == trixie[:name]
+    	p "We have the same name."
+    else
+    	p "Nice to meet you, Trixie. My name is Bobby."
+    end
+
     if bobby[:age] == trixie[:age]
-    	p "Awesome same age!"
+    	p "Awesome, we're the same age."
+    else
+    	p "Sorry, but we aren't the same age."
+	end
+
+	if bobby[:pets] == trixie[:pets]
+		p "Sweet we have pets."
+	else
+		p "Pets can be a handful."
+	end
+
+	if bobby[:color] == trixie[:color]
+		p "Nice we have the same favorite color."
+	else
+		p
+		"Your favorite color is nice as well."
+	end
+
+	if bobby[:name] == jeremy[:name]
+    	p "We have the same name."
+    else
+    	p "Nice to meet you, Jeremy. My name is Bobby."
+    end
+
+    if bobby[:age] == jeremy[:age]
+    	p "Awesome, we're the same age."
+    else
+    	p "Sorry, but we aren't the same age."
+	end
+
+	if bobby[:pets] == jeremy[:pets]
+		p "Sweet we have pets."
+	else
+		p "Pets can be a handful."
+	end
+
+	if bobby[:color] == jeremy[:color]
+		p "Nice we have the same favorite color."
+	else
+		p
+		"Your favorite color is nice as well."
 	end
  end
+
 info()
